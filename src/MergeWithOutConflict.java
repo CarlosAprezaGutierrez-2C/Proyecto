b316b2015d42dafdcfe7d641012870ba3248f98b
Carlos Apreza <20253ds088@utez.edu.mx>
2026-01-15 19:30:13 -0600

    chore: base actividad git merge withoutconflict

---
+++ src/MergeWithOutConflict.java
@@ -0,0 +1,7 @@
+
+   public class MergeWithOutConflict {
+    public static void main(String[] args) {
+        System.out.println("Semana 2");
+        System.out.println("version base");
+    }
+   }
