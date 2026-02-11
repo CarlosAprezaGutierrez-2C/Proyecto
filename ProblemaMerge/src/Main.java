df020fd58d8724ec4c5db044f0da0b58b8112509
Carlos Apreza <20253ds088@utez.edu.mx>
2026-02-10 20:33:28 -0600

    feat: agregar descuento por umbral

--- ProblemaMerge/src/Main.java
+++ ProblemaMerge/src/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
+            final double DESCUENTO = 0.10;
+            final double   UMBRAL_DESCUENTO = 1000.0;
+            double total=0;
+
 
             double subtotal = pedirDouble(scanner, "Subtotal: ");
-            double total = subtotal;
-
+            if (subtotal > UMBRAL_DESCUENTO) {
+               total= subtotal - (subtotal * DESCUENTO);
+            }else{
+                total=subtotal;
+            }
             System.out.printf("Total a pagar: %.2f%n", total);
         }
 
