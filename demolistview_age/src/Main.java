c50e930a7206ee440775ffe8c1666ec16140c5c7
Charlie35 <20253ds088@utez.edu.mx>
2026-03-11 18:56:48 -0600

    demo list view age
    11-03-2026

---
+++ demolistview_age/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String saludo = "Hola mundo";
+        String nombre = "Carlos Apreza Gutierrez";
+        String grupo  = "2C";
+        String fecha  = "11 / 03 / 2026";
+        System.out.println(" MENSAJE: " + saludo);
+        System.out.println(" NOMBRE:  " + nombre);
+        System.out.println(" GRUPO:   " + grupo);
+        System.out.println(" FECHA:   " + fecha);
+
+    }
+}
